6ab27ebdcb71cc937b8f681bed26d669d8e86729
doingnothing <dinglehua@geetest.com>
2015-12-09 16:27:42 +0800

    将sdk index.js更名为gt-sdk.js，版本号为0.5.1

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ var privateKey = '50ed17103f109ccf7c25e93cb6e0d378';
 var publicKey = 'a0ec7ce42d4291382bfc9ca488313b80';
 var api = 'http://api.geetest.com/';
 
-var geetest = require("./sdk/index")(privateKey, publicKey, api);
+var geetest = require("./gt-sdk")(privateKey, publicKey, api);
 
 var app = express();
 
