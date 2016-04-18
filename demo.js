31478c23ecb7d58c2465c39c49ad23e2ef8a228a
NailMan <lehuading@qq.com>
2016-04-18 16:11:04 +0800

    修改demo，添加多实例方法

--- demo.js
+++ demo.js
@@ -4,7 +4,9 @@ var bodyParser = require("body-parser");
 var privateKey = '36fc3fe98530eea08dfc6ce76e3d24c4';
 var publicKey = 'b46d1900d0a894591916ea94ea91bd2c';
 
-var geetest = require("./gt-sdk")(privateKey, publicKey);
+var Geetest = require('./gt-sdk');
+
+var geetest = new Geetest(privateKey, publicKey);
 
 var app = express();
 
@@ -48,11 +50,12 @@ app.post("/validate", function (req, res) {
 
   }, function (err, result) {
 
-    var data = {status: "success"};
+    var data = {status: "success", info: '登录成功'};
 
     if (err || !result) {
 
       data.status = "fail";
+      data.info = '登录失败';
     }
 
     res.send(JSON.stringify(data));
@@ -60,9 +63,26 @@ app.post("/validate", function (req, res) {
   });
 });
 
+app.post("/form-validate", function (req, res) {
+
+  geetest.validate({
+
+    challenge: req.body.geetest_challenge,
+    validate: req.body.geetest_validate,
+    seccode: req.body.geetest_seccode
+
+  }, function (err, result) {
+
+    if (err || !result) {
+      res.send('<h1>登录失败</h1>')
+    } else {
+      res.send('<h1>登录成功</h1>')
+    }
+  });
+});
+
 var port = 8080;
 var server = app.listen(port, function () {
-
   console.log('Example app listening at http://localhost:' + port)
 
 });
--- gt-sdk.js
+++ gt-sdk.js
@@ -4,7 +4,7 @@ var crypto = require('crypto'),
     request = require('request'),
     pkg = require("./package.json");
 
-var apiServer = 'http://api.geetest.com/';
+var apiServer = 'http://api.geetest.com';
 
 var md5 = function (str) {
     return crypto.createHash('md5').update(str).digest('hex');
@@ -30,7 +30,7 @@ Geetest.prototype.validate = function (config, callback) {
 
     if (config.validate === md5(hash)) {
 
-        request.post(apiServer + 'validate.php', {
+        request.post(apiServer + '/validate.php', {
             form: {
                 seccode: config.seccode
             }
@@ -57,7 +57,7 @@ Geetest.prototype.validate = function (config, callback) {
 Geetest.prototype.register = function (callback) {
 
     var self = this;
-    var url = apiServer + 'register.php?gt=' + this.publicKey + '&sdk=Node_' + pkg.version;
+    var url = apiServer + '/register.php?gt=' + this.publicKey + '&sdk=Node_' + pkg.version;
 
     request.get(url, {timeout: 2000}, function (err, res, body) {
 
@@ -74,7 +74,4 @@ Geetest.prototype.register = function (callback) {
     });
 };
 
-module.exports = function (key, id) {
-
-    return new Geetest(key, id);
-};
+module.exports = Geetest;
